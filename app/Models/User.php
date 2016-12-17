c4b6e0261a8155ed55c02dfc971f48d7a490cc54
SadCreeper <87826632@qq.com>
2016-12-17 23:08:17 +0800

    Finish user signup

--- app/Models/User.php
+++ app/Models/User.php
@@ -36,4 +36,25 @@ class User extends Model implements AuthenticatableContract,
      * @var array
      */
     protected $hidden = ['password', 'remember_token'];
+
+    /**
+     * 密码加密
+     *
+     * @var
+     */
+    public function setPasswordAttribute($password)
+    {
+        $this->attributes['password'] = bcrypt($password);
+    }
+
+    /**
+     * gravatar 头像
+     *
+     * @var
+     */
+    public function gravatar($size = '100')
+    {
+        $hash = md5(strtolower(trim($this->attributes['email'])));
+        return "http://www.gravatar.com/avatar/$hash?s=$size";
+    }
 }
